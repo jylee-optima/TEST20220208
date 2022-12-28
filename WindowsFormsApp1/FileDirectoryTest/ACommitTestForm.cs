a10b0e4f67dd43194be857e1a22c52d62e238556
leejeongyong <jylee@optima-1.com>
2022-12-28 11:31:36 +0900

    TEST

---
+++ WindowsFormsApp1/FileDirectoryTest/ACommitTestForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileDirectoryTest
+{
+    public partial class ACommitTestForm : Form
+    {
+        public ACommitTestForm()
+        {
+            InitializeComponent();
+
+            //1
+            //2
+            //3
+            //4
+            //5
+            //6
+            //7
+            //8
+            //9
+            //10
+        }
+    }
+}
